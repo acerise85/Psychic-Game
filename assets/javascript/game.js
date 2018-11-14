af9993faada3f288a9353bbc9054f098f580a32c
acerise <42525915+acerise85@users.noreply.github.com>
2018-11-13 21:51:19 -0700

    played around with for loop, didn't work correctly

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -5,6 +5,7 @@ var win = 0;
 var lose = 0;
 var guesses = 10;
 
+//random letter chooser for computerSelection, working correctly
 var computerSelection = letters[Math.floor(Math.random() * letters.length)];
 
 
@@ -14,43 +15,43 @@ var computerSelection = letters[Math.floor(Math.random() * letters.length)];
 document.onkeyup = function (start){
     console.log(start);
 
+    //start game event and possible userGuess onkeyup event, might need another onkey event, for loop preferable
     var userGuess = start.key.toLocaleLowerCase();
+
+    //aray to store guessed letters
     var lettersChosen = [];
     // var userGuess = document.getElementById("userGuess");
 
-        //Need for loop to keep game looping until the userGuess = 
+        //Need for loop to keep game looping until the userGuess = the computerSelection or guesses == 0, initial comparison works but game does not continue after one onkeyup event
     
         console.log(userGuess);
 
         
-
+        //if statement to see if userGuess matches the computerSelection
         if ( userGuess === computerSelection){
 
             document.write("You guessed correctly!");
 
-            win++;
+            
 
         }
+        //else statment for when userGuess does not match computerSelection
         else {
 
             document.write("Try again!")
 
             lettersChosen.push([]);
 
-            guesses--;
+            
             console.log(computerSelection);
         }
 
         
+            //if statment 
+        // if (guesses === 0){
 
-        if (guesses === 0){
-
-            document.write("Sorry Game Over! The correct letter was: " + computerSelection);
-
+            // document.write("Sorry Game Over! The correct letter was: " + computerSelection);
             
-        }
-
-    
 
 }
 
