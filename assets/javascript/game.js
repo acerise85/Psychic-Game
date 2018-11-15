7927520ea6f983fe257df0d1f97a1d055adf69fe
acerise <42525915+acerise85@users.noreply.github.com>
2018-11-14 21:06:37 -0700

    Javascript correctly looping and displaying everything except for letterChosen array

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -5,55 +5,61 @@ var win = 0;
 var lose = 0;
 var guesses = 10;
 
+
 //random letter chooser for computerSelection, working correctly
 var computerSelection = letters[Math.floor(Math.random() * letters.length)];
 
 
 
+//game start key event
+    document.onkeyup = function (start){
+        console.log(start);
 
-
-document.onkeyup = function (start){
-    console.log(start);
-
-    //start game event and possible userGuess onkeyup event, might need another onkey event, for loop preferable
-    var userGuess = start.key.toLocaleLowerCase();
+//start game event and possible userGuess onkeyup event, might need another onkey event, for loop preferable
+        var userGuess = start.key.toLocaleLowerCase();
 
     //aray to store guessed letters
-    var lettersChosen = [];
-    // var userGuess = document.getElementById("userGuess");
+        var lettersChosen = [""];
 
-        //Need for loop to keep game looping until the userGuess = the computerSelection or guesses == 0, initial comparison works but game does not continue after one onkeyup event
-    
-        console.log(userGuess);
-
-        
+       
         //if statement to see if userGuess matches the computerSelection
         if ( userGuess === computerSelection){
 
-            document.write("You guessed correctly!");
-
-            
-
+            win++;
+            document.getElementById('win').innerHTML = win;
+    
+        
+            console.log(userGuess);
         }
-        //else statment for when userGuess does not match computerSelection
+        //else statment for when userGuess does not match computerSelection the letters go to an array to display letters already chosen
         else {
 
-            document.write("Try again!")
-
             lettersChosen.push([]);
+            
+            lose++;
 
+            document.getElementById("lettersChosen").innerHTML = lettersChosen.toString();
+            document.getElementById('lose').innerHTML = lose;
             
             console.log(computerSelection);
         }
 
         
-            //if statment 
-        // if (guesses === 0){
+    //if statment to subtract number of guesses on wrong answer and let user know they lost round
+        if (userGuess != computerSelection){
 
-            // document.write("Sorry Game Over! The correct letter was: " + computerSelection);
+            guesses--;
+
+            document.getElementById('guesses').innerHTML = guesses;
+
+        }
             
+           if (guesses === 0){
 
-}
+             alert("Sorry, you lose. The correct letter was: " + computerSelection);
+           }
+
+    }
 
 
 
